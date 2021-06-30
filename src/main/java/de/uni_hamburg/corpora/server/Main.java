fbed1d14abb769534aaa24c0932a665a5183a9dd
Lange, Dr. Herbert <herbert.lange@uni-hamburg.de>
2021-06-30 11:26:05 +0200

    add logger

--- src/main/java/de/uni_hamburg/corpora/server/Main.java
+++ src/main/java/de/uni_hamburg/corpora/server/Main.java
@@ -3,6 +3,8 @@ package de.uni_hamburg.corpora.server;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
@@ -15,6 +17,7 @@ public class Main {
     // Base URI the Grizzly HTTP server will listen on
     public static final String BASE_URI = "http://localhost:8080/";
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
@@ -35,10 +38,13 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.logger.warn("Starting server");
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with endpoints available at "
                 + "%s%nHit Ctrl-C to stop it...", BASE_URI));
         System.in.read();
+        main.logger.warn("Shutting down server");
         server.stop();
     }
 }
