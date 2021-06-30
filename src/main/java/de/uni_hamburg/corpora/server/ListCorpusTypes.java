b18489bb2ebe88389394421a05fbcae7b25c1196
Lange, Dr. Herbert <herbert.lange@uni-hamburg.de>
2021-06-30 11:28:15 +0200

    prototype handler to list all supported corpus types

---
+++ src/main/java/de/uni_hamburg/corpora/server/ListCorpusTypes.java
@@ -0,0 +1,35 @@
+package de.uni_hamburg.corpora.server;
+
+import de.uni_hamburg.corpora.CorpusData;
+import org.reflections.Reflections;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.Set;
+
+/**
+ * Root resource (exposed at "myresource" path)
+ */
+@Path("/list_corpus_types")
+public class ListCorpusTypes {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        Reflections reflections = new Reflections("de.uni_hamburg.corpora");
+        Set<Class<? extends CorpusData>> classes = reflections.getSubTypesOf(CorpusData.class);
+        StringBuilder classNames = new StringBuilder() ;
+        for (Class c : classes) {
+            classNames.append(c.getCanonicalName()+"\n");
+        }
+        return classNames.toString();
+    }
+}
