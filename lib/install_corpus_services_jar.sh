641d0d7c25b9a46b994051d36c9fb5a6300d6ed3
Lange, Dr. Herbert <herbert.lange@uni-hamburg.de>
2022-09-22 15:29:37 +0200

    handle redirect when downloading jar

--- lib/install_corpus_services_jar.sh
+++ lib/install_corpus_services_jar.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-curl -o corpus-services-1.0.jar https://gitlab.rrz.uni-hamburg.de/corpus-services/corpus-services/-/jobs/artifacts/develop-quest/raw/target/corpus-services-1.0.jar?job=compile_withmaven
+curl -L -o corpus-services-1.0.jar https://gitlab.rrz.uni-hamburg.de/corpus-services/corpus-services/-/jobs/artifacts/develop-quest/raw/target/corpus-services-1.0.jar?job=compile_withmaven
 mvn install:install-file -Dfile=corpus-services-1.0.jar -DgroupId=de.uni_hamburg.corpora -DartifactId=corpus-services -Dversion=1.0 -Dpackaging=jar
