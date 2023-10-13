5be0885ef1256dde0764c29f2de4b85d9daa8983
Lange, Dr. Herbert <github@pietaetskirsche.de>
2023-10-13 16:51:22 +0200

    update download script

--- lib/install_corpus_services_jar.sh
+++ lib/install_corpus_services_jar.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-curl -L -o corpus-services-1.0.jar https://gitlab.rrz.uni-hamburg.de/corpus-services/corpus-services/-/jobs/artifacts/develop-quest/raw/target/corpus-services-1.0.jar?job=compile_withmaven
-mvn install:install-file -Dfile=corpus-services-1.0.jar -DgroupId=de.uni_hamburg.corpora -DartifactId=corpus-services -Dversion=1.0 -Dpackaging=jar
+# Get the artifact for the most recent artifact from the main branch
+curl -L -o corpus-services-ng-1.0.jar $(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28"  https://api.github.com/repos/daherb/corpus-services-ng/actions/artifacts | python -c "import json; import sys; print([a for a in json.load(sys.stdin)['artifacts'] if a['workflow_run']['head_branch'] == 'main'][0]['url'])")
+mvn install:install-file -Dfile=corpus-services-ng-1.0.jar -DgroupId=de.uni_hamburg.corpora -DartifactId=corpus-services-ng -Dversion=1.0 -Dpackaging=jar
