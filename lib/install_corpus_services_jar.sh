d8eb2a7f43c34ea850463c09d00a8dff09d65c7f
Herbert Lange <github@pietaetskirsche.de>
2023-12-15 11:47:10 +0100

    update corpus-service download script

--- lib/install_corpus_services_jar.sh
+++ lib/install_corpus_services_jar.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 # Get the artifact for the most recent artifact from the main branch
-curl -L -o corpus-services-ng-1.0.jar $(curl -L -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28"  https://api.github.com/repos/daherb/corpus-services-ng/actions/artifacts | python -c "import json; import sys; print([a for a in json.load(sys.stdin)['artifacts'] if a['workflow_run']['head_branch'] == 'main'][0]['url'])")
-mvn install:install-file -Dfile=corpus-services-ng-1.0.jar -DgroupId=de.uni_hamburg.corpora -DartifactId=corpus-services-ng -Dversion=1.0 -Dpackaging=jar
+if [[ -e corpus-services.zip ]] ; then rm corpus-services.zip; fi
+if [[ -e  corpus-services-ng-1.0.jar ]] ; then rm corpus-services-ng-1.0.jar; fi
+wget https://github.com/daherb/corpus-services-ng/releases/latest/download/corpus-services.zip
+unzip corpus-services.zip
+../mvnw install:install-file -Dfile=corpus-services-ng-1.0.jar -DgroupId=de.uni_hamburg.corpora -DartifactId=corpus-services-ng -Dversion=1.0 -Dpackaging=jar
