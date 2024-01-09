b2bedd58eede80dccec083148ec35a2141b13d07
Herbert Lange <github@pietaetskirsche.de>
2024-01-09 16:05:27 +0100

    trying to get things running

--- src/main/java/de/uni_hamburg/corpora/server/Static.java
+++ src/main/java/de/uni_hamburg/corpora/server/Static.java
@@ -1,28 +1,31 @@
 package de.uni_hamburg.corpora.server;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.core.Response;
+//import jakarta.ws.rs.GET;
+//import jakarta.ws.rs.Path;
+//import jakarta.ws.rs.PathParam;
+//import jakarta.ws.rs.core.Response;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.logging.Logger;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.http.MediaType;
 /**
  * @author bba1792 Dr. Herbert Lange
  * @version 20220405
  * Class handling static files
  */
-@Path("/static/{staticFile}")
+
 public class Static {
 
 //    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
     private static final Logger logger = Logger.getLogger(Static.class.getName());
 
-    @GET
-    public Response getStatic(@PathParam("staticFile") String fileName) {
+    @GetMapping("/static/{staticFile}")
+    public ServerResponse getStatic(@PathVariable("staticFile") String fileName) {
         logger.info("Loading file " + fileName);
         try {
             String type;
@@ -35,12 +38,16 @@ public class Static {
             else {
                 type = "application/octet-stream";
             }
-            return Response.ok(this.getClass().getModule().getResourceAsStream("static/" + fileName).readAllBytes())
-                    .type(type)
-                    .build();
+            return ServerResponse
+            		.ok()
+            		.contentType(new MediaType(type))
+            		.body(this.getClass().getModule().getResourceAsStream("static/" + fileName).readAllBytes());
         }
         catch (IOException e) {
-            return Response.status(500, "Error loading resource").build();
+            return // Response.status(500, "Error loading resource").build();
+            		ServerResponse
+            		.status(500)
+            		.body("Error loading resource");
         }
     }
 }
--- src/main/java/de/uni_hamburg/corpora/server/WebRoot.java
+++ src/main/java/de/uni_hamburg/corpora/server/WebRoot.java
@@ -1,22 +1,27 @@
 package de.uni_hamburg.corpora.server;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-
-import jakarta.ws.rs.core.Response;
-//import org.apache.velocity.VelocityContext;
-//import org.apache.velocity.app.Velocity;
-//import org.apache.velocity.Template;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
+//import jakarta.ws.rs.GET;
+//import jakarta.ws.rs.Path;
+//import jakarta.ws.rs.Produces;
+//import jakarta.ws.rs.core.MediaType;
+//
+//import jakarta.ws.rs.core.Response;
+////import org.apache.velocity.VelocityContext;
+////import org.apache.velocity.app.Velocity;
+////import org.apache.velocity.Template;
+////import org.slf4j.Logger;
+////import org.slf4j.LoggerFactory;
+//
+//import java.io.IOException;
 //import java.io.InputStream;
 //import java.io.InputStreamReader;
 import java.io.StringWriter;
 import java.util.logging.Logger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.http.MediaType;
 //import java.net.URL;
 //import java.net.URLClassLoader;
 //import java.util.Arrays;
@@ -27,15 +32,15 @@ import java.util.logging.Logger;
  * @version 20211004
  * Root resource (exposed at "/" path)
  */
-@Path("/")
+@RestController
 public class WebRoot {
 
 //    private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
     private static final Logger logger = Logger.getLogger(WebRoot.class.getName());
 
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public Response getRoot() {
+    @GetMapping("/")
+//    @Produces(MediaType.TEXT_HTML)
+    public ServerResponse getRoot() {
 //        try {
             //putStream template = this.getClass().getModule().getResourceAsStream("templates/root.vm");
 //            logger.info("Test: " + new String(this.getClass().getModule().getResourceAsStream("templates/root.vm")
@@ -45,7 +50,10 @@ public class WebRoot {
 //            context.put("functions", new ListCorpusFunctions().listFunctions());
 //            Velocity.evaluate(context,result,"webroot",new String(this.getClass().getModule()
 //                    .getResourceAsStream("templates/root.vm").readAllBytes()));
-            return Response.ok(result.toString()).build() ;
+            return ServerResponse
+            		.ok()
+            		.contentType(MediaType.TEXT_HTML)
+            		.body(result.toString());
 //        } 
 //        catch (IOException e) {
 //            logger.info("Got exception " + e);
